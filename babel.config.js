707628ff0903f1991b3a6d603867b7b1cfab2969
Piotr Andrzejewski <piotrandrzejewski@MacBook-Pro-Piotr.local>
2019-07-21 16:23:39 +0200

    absolute path config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
+    "plugins": [
+        [
+            "babel-plugin-root-import",
+            {
+                "@common/*": "./src/common/*",
+                "@components/*": "./src/components/*",
+                "@page/*": "./src/page/*",
+                "@api/*": "./src/api/*",
+                "app/*": "./src/app/*",
+                "@assets/*": "./src/assets/*"
+            }
+        ]
+    ],
 };
